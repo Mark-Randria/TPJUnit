a5d33dd0dfbc1bb58dcbf65a0607f7883f22f1b4
Mark-Randria <randrianjafymark2.0@gmail.com>
2024-03-28 23:06:31 +0300

    fix : utilisation de @Before pour eviter la duplication de code

--- src/test/unitaire/classe/MoneyTest.java
+++ src/test/unitaire/classe/MoneyTest.java
@@ -2,14 +2,22 @@ package test.unitaire.classe;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class MoneyTest {
-
+	
+	Money m12CHF;
+	Money m14CHF;
+	
+	@Before
+	public void prepareTest() {
+		m12CHF = new Money(12, "CHF");
+		m14CHF = new Money(14, "CHF");
+	}
+	
 	@Test
 	public void testSimpleAdd() {
-		Money m12CHF = new Money(12, "CHF");
-		Money m14CHF = new Money(14, "CHF");
 		Money expected = new Money(26, "CHF");
 		Money result = m12CHF.add(m14CHF);
 		assertTrue(expected.equals(result));
@@ -17,9 +25,6 @@ public class MoneyTest {
 	
 	@Test
 	public void testEquals() {
-		Money m12CHF = new Money(12, "CHF");
-		Money m14CHF = new Money(14, "CHF");
-		
 		assertTrue(!m12CHF.equals(null));
 		assertEquals(m12CHF, m12CHF);
 		assertEquals(m12CHF, new Money(12, "CHF"));
